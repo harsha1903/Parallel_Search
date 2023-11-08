f43ffcd86afad8da121207659068eb0337c2bb95
Jammigumpula Sai Sriharsha <83920371+harsha1903@users.noreply.github.com>
2023-11-08 08:36:53 +0530

    Update Parallel_Search.c

--- Parallel_Search.c
+++ Parallel_Search.c
@@ -2,12 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
-int a[10000], n = 0, x = 0;
+int a[10000], c[100] = {0}, count = 0, n = 0;
+clock_t start_t, end_t;
+double total_t;
 
-int search(int i) // it will used to search x in array
+void k(int x) // when any one child success in search
+{
+    for (int i = 0; i < n; i++)
+    {
+        kill(c[i], SIGINT);
+        printf("\n\tParent killed the child processs with PID %d by sending signal SIGINT\n", c[i]);
+    }
+
+    printf("\n\tNow the Parent Process died as it's task was completed\n");
+    end_t = clock();
+    total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+    printf("\n\tThe total time taken by the process is:\t%lf\n", total_t);
+    kill(getpid(), SIGINT);
+}
+
+void f(int x) // when search process is compelete but it wont found the x
+{
+    count++;
+    if (count == n)
+    {
+        printf("\n\tThe value x was not found and both the child process sended SIGUSR2 siginal\n");
+        for (int i = 0; i < n; i++)
+        {
+            kill(c[i], SIGINT);
+            printf("\n\tParent killed the child processs with PID %d by sending signal SIGINT\n", c[i]);
+        }
+
+        printf("\n\tParent process dying\n");
+        end_t = clock();
+        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+        printf("\n\t%lf\n", total_t);
+        kill(getpid(), SIGINT);
+    }
+}
+
+void search(int x, int i)
 {
     int v = (10000 / n);
     if ((i + 1) == n)
@@ -16,89 +52,63 @@ int search(int i) // it will used to search x in array
         {
             if (a[j] == x)
             {
-                printf("\n\tThe child process %d with PID %d got success in search\n", i + 1, getpid());
-                return 1;
+                printf("\n\tThe child process %d got success in search\n", i + 1);
+                kill(getppid(), SIGUSR1);
+                return;
             }
         }
-        return 0;
     }
-    for (int j = v * (i); j < v * (i + 1); j++)
+    else
     {
-        if (a[j] == x)
+        for (int j = v * (i); j < v * (i + 1); j++)
         {
-            printf("\n\tThe child process %d with PID %d got success in search\n", i + 1, getpid());
-            return 1;
+            if (a[j] == x)
+            {
+                printf("\n\tThe child process %d got success in search\n", i + 1);
+                // sleep(1);
+                kill(getppid(), SIGUSR1);
+                return;
+            }
         }
     }
-    return 0;
+    kill(getppid(), SIGUSR2);
 }
 
 int main()
 {
-    int status = 0;
+
+    start_t = clock();
+
     srand(time(0));
     for (int i = 0; i < 10000; i++)
     {
         a[i] = rand() % 1000 + 1;
-        // a[i] = i + 1;
+        // a[i]=i+1;
     }
 
-    pid_t p, wpid;
-    printf("Enter the integer x in the range [1-10000] to be searched:\n");
+    signal(SIGUSR1, k);
+    signal(SIGUSR2, f);
+
+    int x;
+    printf("Enter the integer x in the range [1-1000] to be searched:\n");
     scanf("%d", &x);
 
-    printf("Enter the number of childs (n) :\n");
+    printf("Enter the number of childs (n) in the range [1-100]:\n");
     scanf("%d", &n);
-    int c[n], count = 0;
 
+    pid_t p;
     int i;
     for (i = 0; i < n; i++)
     {
-        c[i] = p = fork(); // using fork we create childs
+        p = fork();
+        c[i] = p;
         if (p == 0)
             break;
     }
-    if (p > 0)
-    {
-        while ((wpid = wait(&status)) >= 0) // It means Parent process will wait all child exits
-        {
-            // status is greater than zero means search was successful so we can stop the searching
-            if (status > 0)
-            {
-                printf("\n\tParent synchornzied with child %d death (ret Child PId %d ret Status %d)\n", status / 256, wpid, status);
-                for (int i = 0; i < n; i++)
-                {
-                    if (i != ((status / 256) - 1))
-                    {
-                        kill(c[i], SIGINT);
-                        printf("\n\tParent killed the Child %d PID %d", i + 1, c[i]);
-                    }
-                }
-                break;
-            }
-            else
-            {
-                count++;
-                // when all child process exit code with zero means the element is not present in array
-                if (count == n)
-                {
-                    printf("\n\tThe element was not found in the array\n");
-                }
-            }
-        }
-    }
+    sleep(1);
     if (p == 0)
     {
-        sleep(1);
-        if (search(i))
-        {
-            exit(i + 1); // when searching was success it will exits code with value > 0
-        }
-        else
-        {
-            exit(0); // when searching fails it will exit code with 0
-        }
+        search(x, i);
     }
-    printf("\n");
-    printf("\n\tNow parent dying...as it's task was completed\n");
+    for (;;);
 }
